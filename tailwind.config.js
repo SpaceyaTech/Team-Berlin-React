dcec9b045bc09f24e4c887acd1860336df2f24fb
daveclinton <clintondavid46@gmail.com>
2022-11-20 23:10:52 +0300

    chore: Configure template Paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
