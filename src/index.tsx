b2a59498f44c33d91c29022217138afee2ab05d7
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-11-21 10:10:53 +0300

    Forgot password component

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ForgotPassword from "./ForgotPassword";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <ForgotPassword />
   </React.StrictMode>
 );
 
