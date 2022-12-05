c0f4a4d91db2cbbf19aa6c5b586f3a9291b9e9a1
Byron Kenoly Ochieng <byronochieng01@gmail.com>
2022-12-05 20:19:18 +0300

    removed ForgotPassword import

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import ForgotPassword from "./ForgotPassword";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    {<ForgotPassword />}
-    {/*<App />*/}
+    <App />
   </React.StrictMode>
 );
 
