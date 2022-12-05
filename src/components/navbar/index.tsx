d1c893aae1026d34defbe6568279017cf7399f24
daveclinton <clintondavid46@gmail.com>
2022-12-05 23:14:24 +0300

    feature: Add mobile navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import headerLogo from '../../assets/icons/logo.svg';
 
-const Navbar = () => {
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const toggleNavbar = () => {
+    setIsVisible(!isVisible);
+  };
   return (
-    <div className="w-5/6 m-auto">
+    <div className="w-5/6  m-auto">
       <nav className="flex items-center justify-between flex-wrap bg-white-500 p-10">
         <div className="flex items-center flex-shrink-0 text-white 2xl:mr-12">
           <img src={headerLogo} alt="..." />
@@ -54,10 +60,40 @@ const Navbar = () => {
             Sign
           </button>
         </div>
-        <div className="space-y-2 lg:hidden 2xl:hidden">
+        <div className="space-y-2 lg:hidden 2xl:hidden cursor-pointer" onClick={toggleNavbar}>
           <span className="block w-8 h-1 bg-secondaryGreen"></span>
           <span className="block w-8 h-1 bg-secondaryGreen"></span>
           <span className="block w-8 h-1 bg-secondaryGreen"></span>
+        </div>
+      </nav>
+      <nav className={isVisible ? 'visible container' : 'hidden'}>
+        <div className=" align-middle">
+          <a href="#responsive-header" className="block mt-4  text-darkText ">
+            Home
+          </a>
+          <a href="#responsive-header" className="block mt-4  text-darkText">
+            Forums
+          </a>
+          <a href="#responsive-header" className="block mt-4  text-darkText">
+            Events
+          </a>
+          <a href="#responsive-header" className="block mt-4  text-secondaryGree">
+            Blogs
+          </a>
+          <a href="#responsive-header" className="block mt-4  text-darkText">
+            Jobs
+          </a>
+          <a href="#responsive-header" className="block mt-4  text-darkText ">
+            About Us
+          </a>
+          <div className="flex justify-center">
+            <button className="bg-transparent  text-secondaryGreen font-semibold  py-2 px-4 border  mr-4 rounded">
+              Login
+            </button>
+            <button className="bg-secondaryGreen text-primaryWhite font-semibold  py-2 px-4 border  rounded">
+              Sign
+            </button>
+          </div>
         </div>
       </nav>
     </div>
