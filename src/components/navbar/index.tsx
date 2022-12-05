d2c889cef1df4026696e888b124b9ce521baae25
daveclinton <clintondavid46@gmail.com>
2022-12-05 22:31:42 +0300

    feature: Add hamburger icon

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,37 +5,37 @@ const Navbar = () => {
   return (
     <div className="w-5/6 m-auto">
       <nav className="flex items-center justify-between flex-wrap bg-white-500 p-10">
-        <div className="flex items-center flex-shrink-0 text-white mr-12">
+        <div className="flex items-center flex-shrink-0 text-white 2xl:mr-12">
           <img src={headerLogo} alt="..." />
         </div>
-        <div className="text-2xl">
+        <div className="hidden lg:block">
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12"
+            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12 lg:mr-6"
           >
             Home
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12"
+            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12 lg:mr-6"
           >
             Forums
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12 "
+            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12 lg:mr-6"
           >
             Events
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-secondaryGreen mr-12"
+            className="block mt-4 lg:inline-block lg:mt-0 text-secondaryGreen mr-12 lg:mr-6"
           >
             Blogs
           </a>
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12"
+            className="block mt-4 lg:inline-block lg:mt-0 text-darkText  mr-12 lg:mr-6"
           >
             Jobs
           </a>
@@ -46,13 +46,18 @@ const Navbar = () => {
             About Us
           </a>
         </div>
-        <div>
-          <button className="bg-transparent  text-secondaryGreen font-semibold  py-2 px-4 border w-32  rounded mr-4">
+        <div className="hidden lg:block">
+          <button className="bg-transparent  text-secondaryGreen font-semibold  py-2 px-4 border 2xl:w-32  lg:w-20 rounded mr-4">
             Login
           </button>
-          <button className="bg-secondaryGreen text-primaryWhite font-semibold  py-2 px-4 border w-32 h-12  rounded">
+          <button className="bg-secondaryGreen text-primaryWhite font-semibold  py-2 px-4 border 2xl:w-32  lg:w-20 h-12  rounded">
             Sign
           </button>
+        </div>
+        <div className="space-y-2 lg:hidden 2xl:hidden">
+          <span className="block w-8 h-1 bg-secondaryGreen"></span>
+          <span className="block w-8 h-1 bg-secondaryGreen"></span>
+          <span className="block w-8 h-1 bg-secondaryGreen"></span>
         </div>
       </nav>
     </div>
